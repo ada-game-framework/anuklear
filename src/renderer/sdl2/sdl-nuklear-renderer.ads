09bd3ae7d755b54cf2cfec8e17b226bbbcd5960d
Luke A. Guest <laguest@archeia.com>
2024-10-02 10:59:14 +0100

    Cleaned up. No longer generating this as the script no longer works for this header.

--- src/renderer/sdl2/sdl-nuklear-renderer.ads
+++ src/renderer/sdl2/sdl-nuklear-renderer.ads
@@ -1,95 +1,62 @@
-pragma Ada_2012;
-
-pragma Style_Checks (Off);
-pragma Warnings (Off, "-gnatwu");
-
 with Interfaces.C; use Interfaces.C;
+with SDL.C_Pointers;
+with SDL.Video.Windows;
+with SDL.Video.Renderers;
+with SDL.Events.Events;
+with Nuklear; use Nuklear;
 
-
-with System;
-
-
-with SDL.C_Pointers; 
-with SDL.Video.Windows; 
-with SDL.Video.Renderers; 
-with SDL.Events.Events; 
-with Nuklear; use Nuklear; 
-    
 package SDL.Nuklear.Renderer is
-
-  -- * Nuklear - 4.9.4 - public domain
-  --  
-
-  -- * ==============================================================
-  -- *
-  -- *                              API
-  -- *
-  -- * ===============================================================
-  --  
+   function Initialise
+     (Window   : SDL.Video.Windows.Window;
+      Renderer : SDL.Video.Renderers.Renderer) return access nk_context;
 
 
-   
-   function Initialise 
-     (Window   : SDL.Video.Windows.Window; 
-      Renderer : SDL.Video.Renderers.Renderer) return access nk_context; 
-      
-   function sdl_init (win : SDL.C_Pointers.Windows_Pointer; renderer : SDL.C_Pointers.Renderer_Pointer) return access nk_context  -- ./include/nuklear_sdl_renderer.h:15
-   with Import => True, 
-        Convention => C, 
+   --  NK_API struct nk_context* nk_sdl_init(SDL_Window *win, SDL_Renderer *renderer);
+   function sdl_init (win      : SDL.C_Pointers.Windows_Pointer;
+                      renderer : SDL.C_Pointers.Renderer_Pointer) return access nk_context
+   with Import => True,
+        Convention => C,
         External_Name => "nk_sdl_init";
 
-   
-   procedure sdl_font_stash_begin (atlas : in out font_atlas_access)  -- ./include/nuklear_sdl_renderer.h:16
-   with Import => True, 
-        Convention => C, 
+
+   --  NK_API void nk_sdl_font_stash_begin(struct nk_font_atlas **atlas);
+   procedure sdl_font_stash_begin (atlas : in out font_atlas_access)
+   with Import => True,
+        Convention => C,
         External_Name => "nk_sdl_font_stash_begin";
 
+
+   --  NK_API void nk_sdl_font_stash_end(void);
    procedure sdl_font_stash_end  -- ./include/nuklear_sdl_renderer.h:17
-   with Import => True, 
-        Convention => C, 
+   with Import => True,
+        Convention => C,
         External_Name => "nk_sdl_font_stash_end";
 
-   function sdl_handle_event (evt : in out SDL.Events.Events.Events) return Interfaces.C.int  -- ./include/nuklear_sdl_renderer.h:18
-   with Import => True, 
-        Convention => C, 
+
+   --  NK_API int nk_sdl_handle_event(SDL_Event *evt);
+   function sdl_handle_event (evt : in out SDL.Events.Events.Events) return Interfaces.C.int
+   with Import => True,
+        Convention => C,
         External_Name => "nk_sdl_handle_event";
 
-   procedure sdl_render (arg1 : nk_anti_aliasing)  -- ./include/nuklear_sdl_renderer.h:19
-   with Import => True, 
-        Convention => C, 
+
+   --  NK_API void nk_sdl_render(enum nk_anti_aliasing);
+   procedure sdl_render (arg1 : nk_anti_aliasing)
+   with Import => True,
+        Convention => C,
         External_Name => "nk_sdl_render";
 
-   procedure sdl_shutdown  -- ./include/nuklear_sdl_renderer.h:20
-   with Import => True, 
-        Convention => C, 
+
+   --  NK_API void nk_sdl_shutdown(void);
+   procedure sdl_shutdown
+   with Import => True,
+        Convention => C,
         External_Name => "nk_sdl_shutdown";
 
-  -- Metal API does not support cliprects with negative coordinates or large
-  -- * dimensions. The issue is fixed in SDL2 with version 2.0.22 but until
-  -- * that version is released, the NK_SDL_CLAMP_CLIP_RECT flag can be used to
-  -- * ensure the cliprect is itself clipped to the viewport.
-  -- * See discussion at https://discourse.libsdl.org/t/rendergeometryraw-producing-different-results-in-metal-vs-opengl/34953
-  --  
 
-  -- * ==============================================================
-  -- *
-  -- *                          IMPLEMENTATION
-  -- *
-  -- * ===============================================================
-  --  
-
-  -- setup global state  
-  -- convert from command queue into draw list and draw to screen  
-  -- fill converting configuration  
-  -- convert shapes into vertexes  
-  -- iterate over and execute each draw command  
-  -- warn for cases where NK_SDL_CLAMP_CLIP_RECT should have been set but isn't  
-  -- optional grabbing behavior  
-  -- KEYUP & KEYDOWN share same routine  
-  -- RSHIFT & LSHIFT share same routine  
-  -- MOUSEBUTTONUP & MOUSEBUTTONDOWN share same routine  
-  -- glDeleteTextures(1, &dev->font_tex);  
+   --  NK_API void nk_sdl_handle_grab(void);
+   procedure sdl_handle_grab
+   with Import => True,
+        Convention => C,
+        External_Name => "nk_sdl_handle_grab";
 end SDL.Nuklear.Renderer;
-
-pragma Style_Checks (On);
-pragma Warnings (On, "-gnatwu");
